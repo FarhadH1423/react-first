3235bcf154e32de91ced63ad9f4db0013d387b49
Farhad Hossain <16303038@iubat.edu>
2020-07-18 12:51:19 +0600

    First print something in react!

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <h1 className="display-4">Twinkle Cats</h1>
+      </div>
     </div>
   );
 }
