9d5afc7016f2fb50c54f4c5ec49c06b4dd679483
Farhad Hossain <16303038@iubat.edu>
2020-07-26 21:44:52 +0600

    Print Title Using Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Title  from './Title/Title'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1 className="display-4">Twinkle Cats</h1>
+        <Title />
       </div>
     </div>
   );
